9eab5a2ebf531b46723a1f09deef225f81dccb47
GGBoomSiege <88217512+GGBoomSiege@users.noreply.github.com>
2024-04-20 16:26:46 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,84 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: () => import('../views/HomeView.vue'),
+      redirect: '/home',
+      children: [
+        {
+          path: 'home',
+          name: '主页',
+          component: () => import('../views/children/Home.vue')
+        },
+        {
+          path: 'table',
+          name: 'table',
+          component: () => import('../views/children/Table.vue')
+        }
+      ]
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/login',
+      name: '登录页',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/chart',
+      name: '测试页',
+      component: () => import('../views/Chart.vue')
+    },
+    {
+      path: '/test',
+      name: '测试页',
+      component: () => import('../views/TestView.vue')
+    },
+  ]
+})
+
+// 设置登录过期时间（一天）86400000 
+let expire = 21600000;
+//路由守卫    全局守卫，登录拦截
+//进行路由拦截：当没有登陆标识，直接打回登陆页面，如何避免退回到 登陆页呢？
+
+router.beforeEach((to, from, next) => {
+  // 从本地缓存中获取保存的 token 信息
+  const tokenObj = JSON.parse(window.localStorage.getItem('isLogin'))
+  if (to.path === "/login") {
+    next()
+  } else {
+    // 如果没有token，强制跳转到登录页面；如果有，则判断token时间是否过期
+    if (!tokenObj || !tokenObj.token) {
+      next('/login')
+    } else {
+      let date = new Date().getTime();
+      // 当前时间 - token中的登录时间 > 设置的过期时间，为过期；则清除token，并强制跳转至登录页
+      // 反之为有效期，则放行
+      if (date - tokenObj.startTime > expire) {
+        window.localStorage.removeItem('isLogin');
+        next('/login')
+        ElMessage({
+          message: h('p', null, [
+            h('span', null, '登录状态过期'),
+            h('i', { style: 'color: teal' }, '请重新登录！'),
+          ]),
+        })
+      } else {
+        next();
+      }
+    }
+  }
+});
+
+export default router
---
+++ src/views/TestView.vue
@@ -0,0 +1,47 @@
+<script setup >
+import * as mockjs from "mockjs";
+
+//  mockjs 假数据
+let data1 = mockjs.mock({
+    "data": "@cname()",
+    "age": "@integer(1,100)",
+    "addr": "@city(true)",
+    "email": "@email(qq.com)",
+})
+// console.log(data1);
+
+let data2 = mockjs.mock({
+    "list|5": [
+        {
+            '邮箱': /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
+            '汉字': /[\u4E00-\u9FA5]{3}/,
+            '手机号': /^1[345789]\d{9}$/,
+            'IP地址': /((25[0-5]|2[0-4]\d|[01]\d?)\.){3}(25[0-5]|2[0-4]\d|[01]\d?)/
+        }
+    ]
+})
+// console.log(data2);
+
+let data3 = mockjs.mock({
+    "code": 0,
+    "data|3-5": [
+        {
+            "id|+1": "1",
+            "name": "@cname()",
+            "age": "@integer(1,100)",
+            "addr": "@city(true)",
+            "email": "@email(qq.com)",
+        }
+    ]
+})
+console.log(data3);
+
+</script>
+
+<template>
+    <div>
+        这是测试页面
+    </div>
+</template>
+
+<style lang='scss' scoped></style>
